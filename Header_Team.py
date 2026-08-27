162abeabfca30a5d73ba2bde4686c4c9b92159eb
Vaibhav Vaidish <vaibhav.vaidish.898@gmail.com>
2026-08-27 15:50:11 -0500

    init

---
+++ Header_Team.py
@@ -0,0 +1,17 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Vaibhav Vaidish
+#               Namith Chelikani
+#               Joel Castillo
+#               Kevin Shu
+# Section:      469-569
+# Assignment:   Lab 2
+# Date:         27 08 2026
+#
+
+#
+# YOUR CODE HERE
+#
+
