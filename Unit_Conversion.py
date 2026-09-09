b21089988aeba0861c02cadeef405fc2e0472a9e
Namith Chelikani <142323301+NamithChelikani@users.noreply.github.com>
2026-09-08 22:17:04 -0500

    Add files via upload
    Tell me if there is something you think, I should change. THis is the unit conversion section of the team part

---
+++ Unit_Conversion.py
@@ -0,0 +1,18 @@
+#By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: namith Chelikani, 
+# Section: 469
+# Assignment: lab 03
+# Date: 01 9 2026
+
+x = float(input("Please enter the quantity to be converted:"))
+print(f"{x} pounds force is equivalent to{x*4.448: .3} newtons")
+print(f"{x} meters is equivalent to{x*3.281: .3} feet")
+print(f"{x} atmospheres is equivalent to{x*101.3: .3} kilopascals")
+print(f"{x} watts is equivalent to{x*3.41214: .3} BTU per hour")
+print(f"{x} liters per second is equivalent to{x*15.85: .3} BTU per hour")
+print(f"{x} degrees Celsius is equivalent to{(x*1.8)+32: .3} degrees Fahrenheit")
+
+
