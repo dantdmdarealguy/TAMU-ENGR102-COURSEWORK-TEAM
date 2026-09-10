85c856aa66b98ae2406a11313f08b621088e02f5
Vaibhav Vaidish <vaibhav.vaidish.898@gmail.com>
2026-09-10 15:50:17 -0500

    figured out that when you dont use math you dont have to import it (revelation)

---
+++ Lab 3/unit_conversions.py
@@ -0,0 +1,28 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Vaibhav Vaidish
+#               Namith Chelikani
+#               Joel Castillo
+#               Kevin Shu
+# Section:      469-569
+# Assignment:   Lab 3
+# Date:         08 09 2026
+#
+
+#
+# YOUR CODE HERE
+#
+
+
+
+x = float(input(f"Please enter the quantity to be converted: "))
+print(f"{x:.2f} pounds force is equivalent to{x*4.44822: .2f} newtons")
+print(f"{x:.2f} meters is equivalent to{x*3.28084: .2f} feet")
+print(f"{x:.2f} atmospheres is equivalent to{x*101.325: .2f} kilopascals")
+print(f"{x:.2f} watts is equivalent to{x*3.41214: .2f} BTU per hour")
+print(f"{x:.2f} liters per second is equivalent to{x*15.85033: .2f} US gallons per minute")
+print(f"{x:.2f} degrees Celsius is equivalent to{(x*1.8)+32: .2f} degrees Fahrenheit")
+
+
