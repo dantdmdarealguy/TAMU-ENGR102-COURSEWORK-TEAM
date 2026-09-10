6dae9bcb386931fa16b6a903877fd8a7cd04817d
Namith Chelikani <142323301+NamithChelikani@users.noreply.github.com>
2026-09-10 17:33:04 -0500

    Update still_more_linear_interpolation.py

--- Lab 3/still_more_linear_interpolation.py
+++ Lab 3/still_more_linear_interpolation.py
@@ -1,21 +1,16 @@
-# By submitting this assignment, I agree to the following:
-#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
-#   "I have not given or received any unauthorized aid on this assignment."
+#By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
 #
-# Names:        Vaibhav Vaidish
-#               Namith Chelikani
-#               Joel Castillo
-#               Kevin Shu
-# Section:      469-569
-# Assignment:   Lab 3
-# Date:         08 09 2026
+# Name: namith Chelikani, Kevin Shu, Vaibhav Vaidish, Jeol Castillo
+# Section: 469
+# Assignment: lab 01
+# Date: 8 9 2026
 #
-
 #
 # YOUR CODE HERE
-#
 
-
+import math as m
 
 t1 = float(input("Enter time 1: "))
 x1 = float(input("Enter the x position of the object at time 1: "))
@@ -33,17 +28,17 @@ def interp(time,pos1, pos2):
     point = str((time - t1)*((pos2-pos1)/(t2-t1))+x1)
     return(point)
 
-strx1 = str(x1)
-stry1 = str(y1)
-strz1 = str(z1)
+strx1 = float(x1)
+stry1 = float(y1)
+strz1 = float(z1)
 
-strx2 = str(x1)
-stry2 = str(y1)
-strz2 = str(z1)
+strx2 = float(x1)
+stry2 = float(y1)
+strz2 = float(z1)
 
 
-print("\nAt time",t1,"seconds the object is at ("+strx1+","+stry1+","+strz1+")")
-print("At time",t1+times,"seconds the object is at ("+interp(t1+times,x1,x2)+","+interp(t1+times,y1,y2)+","+interp(t1+times,z1,z2)+")")
-print("At time",t1+(2*times),"seconds the object is at ("+interp(t1+(2*times),x1,x2)+","+interp(t1+times,y1,y2)+","+interp(t1+times,z1,z2)+")")
-print("At time",t1+(3*times),"seconds the object is at ("+interp(t1+(3*times),x1,x2)+","+interp(t1+times,y1,y2)+","+interp(t1+times,z1,z2)+")")
-print("At time",t2,"seconds the object is at ("+strx2+","+stry2+","+strz2+")")
+print(f"\nAt time {t1:.2f} seconds the object is at, ({strx1:.3f}, {stry1:.3f}, {strz1:.3f})")
+print("\nat time",t1+times,"seconds the object is at ("+interp(t1+times,x1,x2)+","+interp(t1+times,y1,y2)+","+interp(t1+times,z1,z2)+")")
+print("\nat time",t1+(2*times),"seconds the object is at ("+interp(t1+(2*times),x1,x2)+","+interp(t1+times,y1,y2)+","+interp(t1+times,z1,z2)+")")
+print("\nat time",t1+(3*times),"seconds the object is at ("+interp(t1+(3*times),x1,x2)+","+interp(t1+times,y1,y2)+","+interp(t1+times,z1,z2)+")")
+print(f"\nat time {t1} seconds the object is at ({strx2:.3f}, {stry2:.3f}, {strz2:.3f})")
